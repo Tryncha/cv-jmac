f0e51fbc024e99c3b158eddf24e6d19b9ee3cb1b
Tryncha <juanes.aguirre26@gmail.com>
2025-10-08 14:19:47 -0500

    modal experimentation & other studies added

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,10 +1,14 @@
+// import { useState } from 'react';
 import educationData from '../../data/education.json';
 import graduatesData from '../../data/graduates.json';
+import otherEducationData from '../../data/otherEducation.json';
 import useSettings from '../../hooks/useSettings';
 import './Education.css';
+// import useModalProperties from '../../hooks/useModalProperties';
 
 const education = educationData.reverse();
 const graduates = graduatesData.reverse();
+const otherEducation = otherEducationData;
 
 const educationTranslation = {
   es: {
@@ -17,55 +21,138 @@ const educationTranslation = {
   }
 };
 
+// interface EducationModalProps {
+//   isOpen: boolean;
+//   closeModal: () => void;
+// }
+
+// const EducationModal = ({ isOpen, closeModal }: EducationModalProps) => {
+//   useModalProperties(isOpen, closeModal);
+
+//   if (!isOpen) return null;
+
+//   return (
+//     <div
+//       className="EducationModal-container"
+//       onClick={closeModal}
+//     >
+//       <article
+//         className="EducationModal"
+//         onClick={(event) => event.stopPropagation()}
+//       >
+//         <button onClick={closeModal}>Close</button>
+//         {otherEducation.map((edu) => (
+//           <div
+//             key={edu.id}
+//             className="Education-item"
+//           >
+//             <div className="Education-mainInfo">
+//               <h3>{edu.name}</h3>
+//               <h4>{edu.institution}</h4>
+//               <span>{edu.city}</span>
+//             </div>
+//             <div className="Education-dateInfo">
+//               <h3>{edu.date}</h3>
+//             </div>
+//           </div>
+//         ))}
+//       </article>
+//     </div>
+//   );
+// };
+
 const Education = () => {
   const { settings } = useSettings();
   const { language } = settings;
 
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // function openModal() {
+  //   setIsModalOpen(true);
+  // }
+
+  // function closeModal() {
+  //   setIsModalOpen(false);
+  // }
+
   return (
-    <article className="Education">
-      <section className="Education-main">
-        <h2 className="u-vitaeSubtitle">{educationTranslation[language].educationTitle}</h2>
+    <>
+      {/* <EducationModal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+      /> */}
+      <article className="Education">
+        <section className="Education-main">
+          <h2 className="u-vitaeSubtitle">{educationTranslation[language].educationTitle}</h2>
+          <hr />
+          {education.map((edu) => (
+            <div
+              key={edu.id}
+              className="Education-item"
+            >
+              <div className="Education-mainInfo">
+                <h3>{edu[language].degreeTitle}</h3>
+                <h4>{edu[language].institution}</h4>
+                <span>{edu[language].city}</span>
+              </div>
+              <div className="Education-awards">
+                <span>{edu[language].award}</span>
+              </div>
+              <div className="Education-dateInfo">
+                <h3>{edu.date}</h3>
+              </div>
+            </div>
+          ))}
+        </section>
+        <section className="Education-graduates">
+          <h2 className="u-vitaeSubtitle">{educationTranslation[language].graduatesTitle}</h2>
+          <hr />
+          {graduates.map((grd) => (
+            <div
+              key={grd.id}
+              className="Education-item"
+            >
+              <div className="Education-mainInfo">
+                <h3>{grd[language].graduateTitle}</h3>
+                <h4>{grd[language].institution}</h4>
+                <span>{grd[language].city}</span>
+              </div>
+              <div className="Education-dateInfo">
+                <h3>{grd[language].date}</h3>
+                <span>{grd[language].duration}</span>
+              </div>
+            </div>
+          ))}
+          {/* <hr />
+          <button onClick={openModal}>
+            <h2 className="u-vitaeSubtitle">Other Education</h2>
+          </button> */}
+        </section>
+      </article>
+      <details>
+        <summary className="u-vitaeSubtitle">Other Studies</summary>
         <hr />
-        {education.map((edu) => (
-          <div
-            key={edu.id}
-            className="Education-item"
-          >
-            <div className="Education-mainInfo">
-              <h3>{edu[language].degreeTitle}</h3>
-              <h4>{edu[language].institution}</h4>
-              <span>{edu[language].city}</span>
+        {otherEducation.map((edu) => (
+          <>
+            <div
+              key={edu.id}
+              className="Education-item"
+            >
+              <div className="Education-mainInfo">
+                <h3>{edu.name}</h3>
+                <h4>{edu.institution}</h4>
+                <span>{edu.city}</span>
+              </div>
+
+              <div className="Education-dateInfo">
+                <h3>{edu.date}</h3>
+                <span>{edu.duration}</span>
+              </div>
             </div>
-            <div className="Education-awards">
-              <span>{edu[language].award}</span>
-            </div>
-            <div className="Education-dateInfo">
-              <h3>{edu.date}</h3>
-            </div>
-          </div>
+          </>
         ))}
-      </section>
-      <section className="Education-graduates">
-        <h2 className="u-vitaeSubtitle">{educationTranslation[language].graduatesTitle}</h2>
-        <hr />
-        {graduates.map((grd) => (
-          <div
-            key={grd.id}
-            className="Education-item"
-          >
-            <div className="Education-mainInfo">
-              <h3>{grd[language].graduateTitle}</h3>
-              <h4>{grd[language].institution}</h4>
-              <span>{grd[language].city}</span>
-            </div>
-            <div className="Education-dateInfo">
-              <h3>{grd[language].date}</h3>
-              <span>{grd[language].duration}</span>
-            </div>
-          </div>
-        ))}
-      </section>
-    </article>
+      </details>
+    </>
   );
 };
 
---
+++ src/hooks/useModalProperties.ts
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+
+function useModalProperties(isOpen: boolean, closeModal: () => void) {
+  useEffect(() => {
+    function handleKeydown(event: KeyboardEvent) {
+      if (event.key === 'Escape' && isOpen) {
+        closeModal();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeydown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, [isOpen, closeModal]);
+
+  useEffect(() => {
+    const scrollY = window.scrollY;
+    const originalStyles = {
+      position: document.body.style.position,
+      top: document.body.style.top,
+      width: document.body.style.width,
+      overflowY: document.body.style.overflowY
+    };
+
+    if (isOpen) {
+      document.body.style.overflowY = 'scroll';
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+      document.body.style.top = `-${scrollY}px`;
+
+      return () => {
+        document.body.style.position = originalStyles.position;
+        document.body.style.top = originalStyles.top;
+        document.body.style.width = originalStyles.width;
+        document.body.style.overflowY = originalStyles.overflowY;
+        window.scrollTo(0, scrollY);
+      };
+    }
+  }, [isOpen]);
+}
+
+export default useModalProperties;
