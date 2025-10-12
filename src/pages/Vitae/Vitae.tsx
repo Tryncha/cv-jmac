8a2c463e93395c6178519eff304bfb6b8a1b5716
Tryncha <juanes.aguirre26@gmail.com>
2025-10-12 18:47:22 -0500

    tailwind & prettier config added

--- src/pages/Vitae/Vitae.tsx
+++ src/pages/Vitae/Vitae.tsx
@@ -3,6 +3,7 @@ import Studies from '../../components/Studies/Studies';
 import WorkExperience from '../../components/WorkExperience/WorkExperience';
 import Direction from '../../components/Direction/Direction';
 import './Vitae.css';
+import Separator from '../../components/Separator/Separator';
 
 const Vitae = () => {
   return (
@@ -11,6 +12,28 @@ const Vitae = () => {
       <Studies />
       <WorkExperience />
       <Direction />
+      <section>
+        <h2>Participaciones como jurado evaluador</h2>
+        <hr />
+        <div className="my-8 flex h-30 gap-5">
+          <div className="flex w-1/4 flex-col gap-0.5">
+            <h3>Universidad Nacional de Colombia</h3>
+            <span>Manizales, Colombia</span>
+            <span>Dic 17, 2019</span>
+          </div>
+          <Separator />
+          <div className="flex w-3/4 flex-col gap-0.5">
+            <span>Maestría en Ingeniería - Ingeniería Química</span>
+            <h3>
+              Biorrefinerias basadas en la conversión catalítica de biomasa para la producción de compuestos fenólicos a
+              partir de residuos de "Eucalyptus grandis" y "Calendula officinalis"
+            </h3>
+            <h4>Estudiante: Jhonny Alejandro Poveda Giraldo</h4>
+            <h4>Director: Carlos Ariel Cardona Alzate</h4>
+            <h4>Rpresentante del Comité Asesor: Ramiro Betancourt Grajales</h4>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
